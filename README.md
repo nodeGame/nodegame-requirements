6c69c7a808656097e824a97d6c231f0b4dddd52f
Stefano Balietti <futur.dorko@gmail.com>
2015-06-01 13:29:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+[nodeGame'](https://www.nodegame.org) requirements checking functions.
+
+Description to be added.
+
+## License
+
+[MIT](LICENSE)
+
+
---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * # nodegame-requirements
+ * Copyright(c) 2015 Stefano Balietti
+ * MIT Licensed
+ *
+ * Collections of requirement-checking functions for connecting clients
+ *
+ * http://www.nodegame.org
+ */
+exports = require('./lib/requirements.js');
+
